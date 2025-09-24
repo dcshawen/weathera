b79d130ef9601564a7f46d7162343dc833e0c76c
Dan Shaw <dcshaw.en@gmail.com>
2025-09-24 14:24:30 -0300

    Begins implementation of Screens

--- app/src/main/java/com/example/weathera/MainActivity.kt
+++ app/src/main/java/com/example/weathera/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.weathera.ui.theme.WeatheraTheme
+import com.example.weathera.ui.screens.CurrentWeather
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeatheraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CurrentWeather()
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatheraTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/weathera/ui/screens/CurrentWeather.kt
+++ app/src/main/java/com/example/weathera/ui/screens/CurrentWeather.kt
@@ -1,2 +1,16 @@
 package com.example.weathera.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun CurrentWeather() {
+    val date = "September 24 2025" // Example date
+    Column {
+        Text(text = "Current Weather Screen")
+        Text(text = "The daily forecast for $date is:")
+    }
+}
