544cf601cc299c75b3ca8a599b414f6cfb5b916c
Dan Shaw <dcshaw.en@gmail.com>
2025-10-03 11:50:18 -0300

    MOBI3002-7 Adds Navigation Scaffold

--- app/src/main/java/com/example/weathera/MainActivity.kt
+++ app/src/main/java/com/example/weathera/MainActivity.kt
@@ -19,20 +19,94 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource
+import com.example.weathera.R
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.NavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             WeatheraTheme {
-                Surface(modifier = Modifier.padding(WindowInsets.systemBars.asPaddingValues())) {
-                    Column {
-                        CurrentWeather()
-                        DailyForecast()
-                    }
-                }
+                DisplayUI()
             }
         }
 
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DisplayUI() {
+    val navController = rememberNavController()
+    var selectedItem by remember { mutableStateOf(0) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Weathera") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+            )
+        },
+        bottomBar = {
+            NavigationBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary
+            )
+            {
+                NavigationBarItem(
+                    label = { Text("Current Weather") },
+                    icon = { Icon(Icons.Filled.Home, contentDescription = "Home") },
+                    selected = selectedItem == 0,
+                    onClick = {
+                        selectedItem = 0
+                        navController.navigate("current")
+                    }
+                )
+                NavigationBarItem(
+                    label = { Text("Forecast") },
+                    icon = { Icon(Icons.Filled.Settings, contentDescription = "Settings") },
+                    selected = selectedItem == 1,
+                    onClick = {
+                        selectedItem = 1
+                        navController.navigate("forecast")
+                    }
+                )
+            }
+        }
+    )
+    {
+        innerPadding ->
+
+        NavHost(
+            navController = navController,
+            startDestination = "current",
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable("current") { CurrentWeather() }
+            composable("forecast") { DailyForecast() }
+        }
     }
 }
