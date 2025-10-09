d6ac8cdb87ae9306b9d9c325d00f56ae533fc8b3
Dan Shaw <dcshaw.en@gmail.com>
2025-10-09 20:48:16 -0300

    Defines Weather Model

--- app/src/main/java/com/example/weathera/models/Weather.kt
+++ app/src/main/java/com/example/weathera/models/Weather.kt
@@ -1,2 +1,31 @@
 package com.example.weathera.models
 
+data class Weather(
+   val current: CurrentWeather,
+   val forecast: List<Forecast>
+)
+
+data class CurrentWeather(
+    val temperature: String,
+    val condition: String,
+    val precipitationChance: String,
+    val precipitationType: String,
+    val precipitationAmount: String,
+    val windDirection: String,
+    val windSpeed: String,
+    val weatherImage: Int
+)
+
+data class Forecast(
+    val date: String,
+    val weatherImage: Int,
+    val highTemp: String,
+    val lowTemp: String,
+    val condition: String,
+    val precipitationChance: String,
+    val precipitationType: String,
+    val precipitationAmount: String,
+    val windDirection: String,
+    val windSpeed: String,
+    val humidity: String
+)
